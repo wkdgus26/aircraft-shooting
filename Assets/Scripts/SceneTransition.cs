9fa011064e4eae4ff8a00cec89389de1f5cac06d
wkdgus26 <51107877+wkdgus26@users.noreply.github.com>
2020-11-09 02:34:05 +0900

    11/9
    11/9

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ButtonClick(string idx)
+	{
+		switch (idx)
+		{
+			case "1P":
+				SceneManager.LoadScene("PlayScene");
+				break;
+			case "2P":
+				Debug.Log("2");
+				break;
+			case "Rank":
+				Debug.Log("rank");
+				break;
+			case "Exit":
+				Debug.Log("exit");
+				break;
+		}
+	}
+}
